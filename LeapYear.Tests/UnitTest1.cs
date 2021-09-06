790e511b459ac3a175db3df24b49d06606230eaf
AmalieBoegild <abso@itu.dk>
2021-09-06 09:55:20 +0200

    Tested and implemented IsLeapYear

--- LeapYear.Tests/UnitTest1.cs
+++ LeapYear.Tests/UnitTest1.cs
@@ -9,11 +9,30 @@ namespace LeapYear.Tests
         [Fact]
         public void TestDivisibleByFour()
         {
-            Assert.Equal(true, Program.IsLeapYear(8));
-            Assert.Equal(true, Program.IsLeapYear(4));
-            Assert.Equal(true, Program.IsLeapYear(0));
-            Assert.Equal(false, Program.IsLeapYear(13));
-            Assert.Equal(false, Program.IsLeapYear(18));
+            Assert.True(Program.IsLeapYear(8));
+            Assert.True(Program.IsLeapYear(4));
+            Assert.True(Program.IsLeapYear(0));
+            Assert.False(Program.IsLeapYear(13));
+            Assert.False(Program.IsLeapYear(18));
+        }
+
+        [Fact]
+        public void TestDivisibleByHundred()
+        {
+            Assert.False(Program.IsLeapYear(200));
+            Assert.False(Program.IsLeapYear(100));
+            Assert.True(Program.IsLeapYear(0));
+            Assert.True(Program.IsLeapYear(16));
+            Assert.False(Program.IsLeapYear(1005));
+        }
+
+        [Fact]
+        public void TestDivisibleByFourhoundred()
+        {
+            Assert.True(Program.IsLeapYear(2000));
+            Assert.True(Program.IsLeapYear(1600));
+            Assert.False(Program.IsLeapYear(200));
+            Assert.False(Program.IsLeapYear(1800));
         }
     }
 }
--- LeapYear/Program.cs
+++ LeapYear/Program.cs
@@ -8,6 +8,14 @@ namespace LeapYear
         {
             if (year % 4 == 0)
             {
+                if (year % 100 == 0)
+                {
+                    if (year % 400 == 0)
+                    {
+                        return true;
+                    }
+                    return false;
+                }
                 return true;
             }
             return false;
